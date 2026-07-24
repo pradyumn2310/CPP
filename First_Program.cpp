184f67579affd3ec27581b2cbc2a482f0750b458
pradyumn2310 <111836722+pradyumn2310@users.noreply.github.com>
2026-07-24 05:37:55 +0530

    Add files via upload

---
+++ First_Program.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"Log.h"
+using namespace std;
+int main(){
+int a;
+int b;
+
+cout<<"Enter a of your choice:";
+cin>>a;
+cout<<"Enter b of your choice:";
+cin>>b;
+
+Multiply(a,b);
+}

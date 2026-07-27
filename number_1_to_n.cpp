2a29b32ec9e79b0dd0772729779341e361db5269
pradyumn2310 <111836722+pradyumn2310@users.noreply.github.com>
2026-07-28 05:08:32 +0530

    Add files via upload

---
+++ number_1_to_n.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the number you want to print the series upto:";
+    cin>>a;
+for(int i=1;i<=a;i++){
+    cout<<i<<endl;
+
+}
+return 0;
+}

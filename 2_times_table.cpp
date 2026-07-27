575257c7ff8eebb63316037441aaa534cf2efa3d
pradyumn2310 <111836722+pradyumn2310@users.noreply.github.com>
2026-07-28 05:07:56 +0530

    Add files via upload

---
+++ 2_times_table.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number you want the product table of:";
+    cin>>a;
+    for(int i=0;i<=9;i++){
+        cout<<a*(i+1)<<endl;
+
+    }
+return 0;}

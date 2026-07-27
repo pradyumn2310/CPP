6ea30dab1a055fdf618991f8abf6a5f2dd632983
pradyumn2310 <111836722+pradyumn2310@users.noreply.github.com>
2026-07-27 05:42:19 +0530

    Add files via upload

---
+++ basic_arithmatic_operations.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+//first operation= sum
+cout<<"Enter a:";
+cin>>a;
+cout<<"Enter b:";
+
+cin>>b;
+int sum=a+b;
+cout<<"The sum of the numbers are:"<<sum<<endl;
+
+//now the operation is subtraction
+int sub=a-b;
+cout<<"The subtraction of the numbers is:"<<sub<<endl;
+
+//now the operation for multiplication
+int product=a*b;
+cout<<"The product of the numbers is:"<<product<<endl;
+
+//now the operation for division
+
+long div=a/b;
+cout<<"The division for the numbers is:"<<div<<endl;
+}
---
+++ celcius_to_farenheit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int celcius;
+cout<<"Enter the temperature in celius:";
+cin>>celcius;
+
+float farenhiet;
+farenhiet=(celcius*(9.0/5.0))+32;
+
+cout<<"Farenheit="<<farenhiet;
+
+}

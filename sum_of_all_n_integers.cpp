b88c3295bab82601eff59a5cb9841e3650ff4aa2
pradyumn2310 <111836722+pradyumn2310@users.noreply.github.com>
2026-07-28 05:07:37 +0530

    Add files via upload

---
+++ sum_of_all_n_integers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int sum=0;
+int n;
+cout<<"Enter n:";
+cin>>n;
+for(int i=0;i<=n;i++)
+    {
+         sum+=i;
+    }
+    cout<<sum;
+    }
+
